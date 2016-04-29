3acfc8630aa5cd3d42cded3097cff79d26bb2124
premkumaranand <aa.premkumar@gmail.com>
2016-04-29 10:00:27 +0100

    Init

---
+++ ArrayExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tekhub.Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static T[][] Split<T>(this T[] arrayIn, int length)
+        {
+            bool even = arrayIn.Length % length == 0;
+            int totalLength = arrayIn.Length / length;
+            if (!even)
+                totalLength++;
+
+            T[][] newArray = new T[totalLength][];
+            for (int i = 0; i < totalLength; ++i)
+            {
+                int allocLength = length;
+                if (!even && i == totalLength - 1)
+                    allocLength = arrayIn.Length % length;
+
+                newArray[i] = new T[allocLength];
+                Array.Copy(arrayIn, i * length, newArray[i], 0, allocLength);
+            }
+            return newArray;
+        }
+    }
+}
